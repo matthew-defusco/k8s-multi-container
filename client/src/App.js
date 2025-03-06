16f7a9d5cc83fa0f6a8b71936437703055fc7bc7
Matt <matthew.defusco@gmail.com>
2025-03-06 08:57:35 -0500

    updating README

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello from Kubernetes!</p>
+          <p>Hello from Kubernetes!!</p>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
